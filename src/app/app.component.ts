33b425ebb5eb3d177d187c81e5a7cb5a2d1cdd10
Wayne <na>
2017-01-17 16:37:59 -0600

    initial checkin

---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
+
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = Page1;
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Page One', component: Page1 },
+      { title: 'Page Two', component: Page2 }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+
+      //initialize ezAR videoOverlay plugin and start back camera
+      var win: any = window;
+       if (win.ezar) {
+            var ezar: any = win.ezar;
+            ezar.initializeVideoOverlay(
+                function() {
+                 ezar.getBackCamera().start();
+                },
+                  function(err) {
+                alert('unable to init ezar: ' + err);
+            });
+        } else {
+            alert('Unable to detect the ezAR plugin');
+        }
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
---
+++ src/pages/page2/page2.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-page2',
+  templateUrl: 'page2.html'
+})
+export class Page2 {
+  selectedItem: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+  }
+
+}
